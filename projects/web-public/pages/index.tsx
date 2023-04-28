9f12f190a8b1d82f99fd3086af8b2e70e852cd9c
Eduardo <eduardo.fontes.lourenco@gmail.com>
2023-04-28 14:43:43 -0300

    test first monorepo package import

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,7 +1,10 @@
+import { sum } from '@eduardofl/utils/math/sum';
+
 export default function HomeScreen() {
     return (
       <main>
-        Home screen!
+        <h1>Home screen!</h1>
+        <p>Testando a função sum do package utils - sum(157, 456): { sum(157, 456) }</p>
       </main>
     );
 }
