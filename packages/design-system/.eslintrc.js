39c2312d291a44dd663b04447499a6200fdf6e63
Eduardo <eduardo.fontes.lourenco@gmail.com>
2023-05-04 11:47:42 -0300

    add eslint and prettier to the monorepo

---
+++ packages/design-system/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require("@eduardofl/eslint-commons/base");
--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Text } from '@eduardofl/design-system/components/Text';
-import { sum } from '@eduardofl/utils/math/sum';
+import { Text } from "@eduardofl/design-system/components/Text";
+import { sum } from "@eduardofl/utils/math/sum";
 
 export default function HomeScreen() {
-    return (
-      <main>
-        <Text tag="h1">Home screen!</Text>
-        <Text tag="p">Testando a função sum do package utils - sum(157, 456): { sum(157, 456) }</Text>
-      </main>
-    );
+  return (
+    <main>
+      <Text tag="h1">Home screen!</Text>
+      <Text tag="p">
+        Testando a função sum do package utils - sum(157, 456): {sum(157, 456)}
+      </Text>
+    </main>
+  );
 }
